0db8083fa02015adbac9a1469551cf91a4fce249
ПК <ekrapostin@gmail.com>
2023-05-18 19:20:49 +0300

    Выполнил дз по Spring

---
+++ src/main/java/Transport.java
@@ -0,0 +1,15 @@
+public abstract class Transport {
+
+    private String model;
+
+    public Transport(String model) {
+        this.model = model;
+    }
+
+    @Override
+    public String toString() {
+        return "Transport{" +
+                "model='" + model + '\'' +
+                '}';
+    }
+}
